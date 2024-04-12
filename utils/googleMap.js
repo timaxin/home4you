e9786d2066208c6e8f2951dfd465c728eafeafd0
Tsimafei Yefimenka <tsimafei.yefimenka@comparethemarket.com>
2024-04-12 13:14:51 +0200

    HY-2: Refactor googleMap.js

--- utils/googleMap.js
+++ utils/googleMap.js
@@ -1,14 +1,13 @@
-import {useRef, useEffect, useMemo, useState} from 'react';
+import {useRef, useEffect} from 'react';
 import loader from './googleMapsLoader';
 
-const Map = ({ address, className }) => {
-  const [map, setMap] = useState(null);
+const Map = ({address, className}) => {
   const mapRef = useRef(null);
   useEffect(() => {
     loader.load().then(() => {
       const geocoder = new window.google.maps.Geocoder();
       geocoder.geocode({address}, (results, status) => {
-        if(status === 'OK') {
+        if (status === 'OK') {
           const mapOptions = {
             center: results[0].geometry.location,
             zoom: 15,
@@ -17,11 +16,10 @@ const Map = ({ address, className }) => {
             mapRef.current,
             mapOptions
           );
-          const marker = new window.google.maps.Marker({
+          new window.google.maps.Marker({
             position: mapOptions.center,
             map: newMap,
           });
-          setMap(newMap);
         } else {
           console.error(`Address ${address} cannot be geocoded`);
         }
